88063cf6499dfe034fb893316ef2f0210a2edcf8
Aman Gupta <aman@tmm1.net>
2017-05-09 15:20:19 -0700

    Disable verbose logging of unexpected ssdp responses

--- ssdp/ssdp.go
+++ ssdp/ssdp.go
@@ -60,7 +60,7 @@ func SSDPRawSearch(httpu *httpu.HTTPUClient, searchTarget string, maxWaitSeconds
 			continue
 		}
 		if st := response.Header.Get("ST"); st != searchTarget {
-			log.Printf("ssdp: got unexpected search target result %q", st)
+			//log.Printf("ssdp: got unexpected search target result %q", st)
 			continue
 		}
 		location, err := response.Location()
