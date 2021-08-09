9fa241017d6244b5ace984b3f0c6366770d3edee
humboldt_xie <humboldt_xie@163.com>
2021-08-10 01:25:57 +0800

    fix <controlURL>_urn:schemas-upnp-org:service:AVTransport_control</controlURL>  throw error first path segment in URL cannot contain colon

--- device.go
+++ device.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/huin/goupnp/scpd"
 	"github.com/huin/goupnp/soap"
@@ -178,6 +179,11 @@ type URLField struct {
 }
 
 func (uf *URLField) SetURLBase(urlBase *url.URL) {
+	str := uf.Str
+	if !strings.Contains(str, "://") && !strings.HasPrefix(str, "/") {
+		str = "/" + str
+	}
+
 	refUrl, err := url.Parse(uf.Str)
 	if err != nil {
 		uf.URL = url.URL{}
